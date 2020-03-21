51eecd12099609d1add2219fb25b253cab4f8f99
Rohan Batra <rohanbatra24@gmail.com>
2020-03-21 16:10:40 +0000

    tree traversal practice

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let counter = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      counter++;
+    }
+
+    return counter;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -35,10 +44,7 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  closestCommonAncestor(vampire) {}
 }
 
 module.exports = Vampire;
-
